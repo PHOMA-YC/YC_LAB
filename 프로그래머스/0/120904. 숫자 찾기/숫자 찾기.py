5cc9acbdc57ed8156601ab4bea889ef230fdcddb
PHOMA-YC <162594928+PHOMA-YC@users.noreply.github.com>
2024-05-29 12:47:13 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -0,0 +1,9 @@
+def solution(num, k):
+    num = str(num)
+    for i in range(len(num)) :
+        if int(num[i]) == k :
+            return i + 1
+        elif str(k) not in num :
+            return -1
+        
+            
