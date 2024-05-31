14eda1479f5cfcc87044b572bcb83170525a3240
PHOMA-YC <162594928+PHOMA-YC@users.noreply.github.com>
2024-05-31 11:07:23 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,18 +1,2 @@
 def solution(before, after):
-    result = []
-    before = list(before)
-    after = list(after)
-    for i in before :
-        if i in after :
-            result.append(1)
-            after.remove(i)
-        else :
-            result.append(0)
-        
-    if result.count(1) == len(before) :
-        return 1
-    else :
-        return 0
-        
-        
-            
+    return 1 if str(before[::-1]) == after else 0
