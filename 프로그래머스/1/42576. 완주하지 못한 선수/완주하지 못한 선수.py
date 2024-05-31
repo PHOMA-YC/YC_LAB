c25404ac3e55b3aab4174c041ea753c68afd3267
PHOMA-YC <162594928+PHOMA-YC@users.noreply.github.com>
2024-05-31 17:04:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.42 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,28 +1,12 @@
-# def solution(participant, completion):
-    
-#     for i in range(len(participant)) :   
-#       isData=0 # 없다   
-#       for j in range(len(completion)) :
-#         if participant[i]==completion[j] :
-#            isData=1 # 있다 
-#            completion.remove(participant[i])
-#            break
-        
-#       if isData==0 :
-#         return participant[i]     
-    
-    
-    
-
-    
 def solution(participant, completion):
-
-    p_list = sorted(participant)
-    c_list = sorted(completion)
     
-
-    for p, c in zip(p_list, c_list):
-        if p != c:
-            return p
+    for i in range(len(participant)) :   
+      isData=0 # 없다   
+      for j in range(len(completion)) :
+        if participant[i]==completion[j] :
+           isData=1 # 있다 
+           completion.remove(participant[i])
+           break
         
-    return p_list[-1]
+      if isData==0 :
+        return participant[i]     
