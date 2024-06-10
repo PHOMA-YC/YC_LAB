e1a740d32ba6a67e83a5e6f71939401407b6e62a
PHOMA-YC <162594928+PHOMA-YC@users.noreply.github.com>
2024-06-10 11:45:26 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,9 @@
 def solution(citations):
+    citations = sorted(citations)
     n = 0
     while True :
         if n <= len([i for i in citations if i >= n]) :
             n += 1
         else :
             break
-    
     return n - 1
