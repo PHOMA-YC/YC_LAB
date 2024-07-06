71227a47d73a0bd2c165d625ea17ce7faf4ea937
PHOMA-YC <162594928+PHOMA-YC@users.noreply.github.com>
2024-07-06 10:49:09 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,19 +1,17 @@
 def solution(food):
-    food_list = [1]
+    food_copy = list(map(lambda x: x//2, food)) #--> i는 음식, idx는 먹는음식의 갯수
+    answer = ''
     result = ''
-    
-    for i in food[1:]:
-        if i % 2 == 0 :
-            food_list.append(i)
-        else:
-            food_list.append(i-1)
-            
-    for idx, i in enumerate(food_list[1:]):
-        if i == 0 :
+    # 반 짤라서 앞배열 생성 후 뒤집어서 붙여
+    for idx, i in enumerate(food):
+        if i <= 1:
             continue
-        else :        
-            result += str(idx+1) * (i // 2)
-        
-    reverse_result = result[::-1]
-    
-    return result + '0' + reverse_result
+        else:
+            answer += str(idx) * food_copy[idx]
+            
+    result += answer
+    result += '0'
+    result += answer[::-1]
+
+    return result
+            
