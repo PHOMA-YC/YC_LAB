a2b685cbc5cff6684fa8f8063df4709903a8e830
PHOMA-YC <162594928+PHOMA-YC@users.noreply.github.com>
2024-07-16 18:10:53 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    # new_num = []
+    # while len(new_num) < 5:
+    #     min_num = min(num_list)
+    #     new_num.append(min_num)
+    #     num_list.remove(min_num)
+    # sorted_remain = sorted(num_list)
+    # print(sorted_remain)
+    
+    return sorted(num_list)[5:]
+    
+
+                
